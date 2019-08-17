3bd28246665360467329105a82cdea79c0a24ff5
Gleb Dubinin <dubining10@gmail.com>
2019-08-17 09:52:29 +0300

    i2c

--- initialization.cpp
+++ initialization.cpp
@@ -74,7 +74,7 @@ NiFpga_Status initHardware(NiFpga_Status* status, MyRio_I2c* i2cA, MyRio_Dio* Bu
 //	else
 //		I2c_Counter(i2cA, 187);
 	
-	I2c_Counter(i2cA, 187);
+	I2c_Counter(i2cA, 255);
 
 	/*
 	 * Enable the I2C block.
